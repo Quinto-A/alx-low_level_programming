ded96cce15f8856411f0c5bc312675c4b4f15343
Acquino Quinto <acquinoquinto5@gmail.com>
2023-10-29 13:16:23 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main- print all the arguments passed
+ * @argc: number of arguments
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
